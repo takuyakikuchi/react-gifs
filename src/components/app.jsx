f041277210ea7999d79dea6afcae5efa326acfb6
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2019-12-13 15:50:45 +0900

    added git_list components

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
 
 class App extends Component {
   render() {
+    const gifs = [
+      { id: "3o8dpbZRKpIjdNgQHm" },
+      { id: "3oz8xALpV1X2BPo7cI" }
+    ];
     return (
       <div>
         <div className="left-scene">
@@ -12,7 +17,9 @@ class App extends Component {
             <Gif id='3o8dpbZRKpIjdNgQHm' />
           </div>
         </div>
-        <div className="right-scene"></div>
+        <div className="right-scene">
+          <GifList gifs={gifs} />
+        </div>
       </div>
     );
   }
