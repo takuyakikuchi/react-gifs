2b1eb54f6b7dd9de31971feeeccfe49e560a341c
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2019-12-13 16:48:10 +0900

    added input event triggers api search

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
+import giphy from 'giphy-api';
+
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
 import GifList from './gif_list.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gifs: [],
+      selectedGifId: "3o8dpbZRKpIjdNgQHm"
+    }
+  }
+
+  search = (query) => {
+    giphy('A1CbGjRLvNLqYJ8trmONqdkgTkasbMHd').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (err, res) => {
+      this.setState({
+        gifs: res.data
+      });
+    })
+  }
+
   render() {
-    const gifs = [
-      { id: "3o8dpbZRKpIjdNgQHm" },
-      { id: "3oz8xALpV1X2BPo7cI" }
-    ];
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar search={this.search} />
           <div className="selected-gif">
-            <Gif id='3o8dpbZRKpIjdNgQHm' />
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={gifs} />
+          <GifList gifs={this.state.gifs} />
         </div>
       </div>
     );
--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import Gif from './gif.jsx';
 
 class GifList extends Component {
+  renderList = () => {
+    return this.props.gifs.map(gif => <Gif id={gif.id} key={gif.id} />);
+  }
+
   render() {
-    return this.props.gifs.map(gif => {
-      return <Gif id={gif.id} key={gif.id} />;
-    });
+    return (
+      <div className="gif-list">
+        {this.renderList()}
+      </div>
+    );
   }
 }
 
